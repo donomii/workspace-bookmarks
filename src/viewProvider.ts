7c9bc8c1937f2a0805cd535e226db155ce1861cc
Jeremy Price <donomii@gmail.com>
2025-05-29 19:47:52 +0900

    fucked

--- src/viewProvider.ts
+++ src/viewProvider.ts
@@ -2,26 +2,44 @@ import * as vscode from "vscode";
 import * as path from "path";
 import { Bookmark, BookmarkManager } from "./bookmarks";
 
-export class BookmarkTreeProvider implements vscode.TreeDataProvider<BookmarkItem> {
-  private _onDidChangeTreeData = new vscode.EventEmitter<BookmarkItem | undefined>();
+class CommandItem extends vscode.TreeItem {
+  constructor(label: string, command: string, icon: string) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+    this.command = {
+      command,
+      title: label,
+    };
+    this.iconPath = new vscode.ThemeIcon(icon);
+  }
+}
+
+export class BookmarkTreeProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
+  private _onDidChangeTreeData = new vscode.EventEmitter<vscode.TreeItem | undefined>();
   readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
 
   constructor(private manager: BookmarkManager) {
     this.manager.onDidChange(() => this.refresh());
   }
 
-  getTreeItem(element: BookmarkItem): vscode.TreeItem {
+  getTreeItem(element: vscode.TreeItem): vscode.TreeItem {
     return element;
   }
 
-  getChildren(): BookmarkItem[] {
-    return this.manager.getAll().map(
-      (b) =>
-        new BookmarkItem(
-          `${path.basename(b.uri.fsPath)}:${b.line + 1}  ${b.preview}`,
-          b
-        )
-    );
+  getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
+    if (element) {
+      return [];
+    }
+    return [
+      new CommandItem("➕ Add from Cursor", "workspaceBookmarks.addFromCursor", "add"),
+      new CommandItem("🗑️ Clear All Bookmarks", "workspaceBookmarks.clearAll", "trash"),
+      ...this.manager.getAll().map(
+        (b) =>
+          new BookmarkItem(
+            `${path.basename(b.uri.fsPath)}:${b.line + 1}  ${b.preview}`,
+            b
+          )
+      )
+    ];
   }
 
   refresh(): void {
@@ -38,5 +56,6 @@ class BookmarkItem extends vscode.TreeItem {
       arguments: [this.bookmark],
     };
     this.iconPath = new vscode.ThemeIcon("bookmark");
+    this.contextValue = "workspaceBookmarkItem";
   }
 }
